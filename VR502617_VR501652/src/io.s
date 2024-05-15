2c1f92bae956fbc044617c4b1f0219b4f0e42f96
Rick-1242 <ricciueb@gmail.com>
2024-05-15 16:45:17 +0200

    Added get parms funcion and therefore a print funcion(this needs modifica)-> printf

--- VR502617_VR501652/src/io.s
+++ VR502617_VR501652/src/io.s
@@ -1,20 +1,70 @@
 .section .data
 
+new_line_char:
+	.byte 10
+
 .section .text
-	.global readCLI
+	.global getParms
 
-.type readCLI, @function		# dichiarazione della funzione itoa che
-							# convert un intero in una stringa
-							# il numero da convertire deve essere
-							# stato caricato nel registro %eax
+.type getParms, @function		# Dicardo la funzione getParms
+                                # al momento stmapa i parametri ottenuti in esecuzione 
+                                # takes ecx  and prints it
 
-readCLI:  
+                                # TODO in futuro li da indietro
 
-writeCLI:
 
-readF:
+getParms:               # elimino i primi due valori nello stack
+	popl %ecx			# Contiene il numero dei parametri totali
+	popl %ecx
 
-writeF:
+handle_par:
+	popl %ecx			# Recupera l'indirizzo della stringa relativa al  parametro
+	testl %ecx, %ecx	# controlla se EAX =  0 (NULL)
+	jz fine
+	call print_par		# stampa il parametro
+	jmp handle_par 		# ciclo nuovamente 
 
+fine:
+	movl $1, %eax
+	movl $0, %ebx
+	int $0x80
+
+
+# --------------------------------------
+.type print_par, @function			# Stampa la stringa del parametro e va a capo
+print_par:
+	call count_char 												
+	movl $4, %eax
+	movl $1, %ebx					
+									# Non serve settare ecx ed edx in quanto
+									# l'indirizzo è già contenuto in ecx
+									# la lunghezza della stringa è già contenuta in edx
+	int $0x80
+
+	movl $4, %eax
+	movl $1, %ebx
+	leal new_line_char, %ecx
+	movl $1, %edx
+	int $0x80
+	
+	ret
+
+
+# --------------------------------------
+.type count_char, @function			# conta da quanti caratteri è composta la stringa del parametro
+									# il valore risultato è contenuto in edx
+count_char:
+	xorl %edx, %edx
+
+iterate:
+	movb (%ecx,%edx), %al 			# mette il carattere della stringa in al
+	testb %al, %al 					# se il carattere è 0 (\0) la stringa è finita
+	jz end_count				
+	incl %edx
+	jmp iterate
+
+end_count:
+	ret
+	
 
 
--- VR502617_VR501652/src/main.s
+++ VR502617_VR501652/src/main.s
@@ -1,10 +1,5 @@
 .section .data
 
-ordiniArr:
-	.long 1, 2, 3, 4  
-	.long 5, 6, 7, 8   
-	.long 9, 10, 11, 12  
-
 OBJECT_SIZE = 4   # Numero di interi(elemnti) per oggeto(ordine) 4 elementi x 4 byte = 16 byte a oggetto
 
 IDENTIFICATIVO_OFFSET = 0
@@ -13,21 +8,17 @@ SCANDEZA_OFFSET = 8
 PRIORITA_OFFSET = 12
 
 .section .bss
-#ordiniArr: 
-    #.fill 40, 4, 0 # crete 40 4 bytre entries wiht 0 that will be modified by funcions in IO.s
+ordiniArr: 
+    .fill 40, 4, 0 # crete 40 4 byte entries wiht 0 that will be modified by funcions in IO.s
 
 
 .section .text
 	.global _start
 
 _start:
-	movl $1, %eax  # eax serve da indice per l' array ordini arr, poi andara in .bss ma mi sto disperando quindi sto provando anche questa
-	leal ordiniArr(,%eax, 4), %ecx # Access first element of array eatch elemnt is 4 bytes
 
-	movl $4, %eax              # syscall number for sys_write
-    movl $1, %ebx              # file descriptor 1 (stdout)
-    movl $4, %edx              # number of bytes to print
-    int $0x80                  # call kernel
+	call getParms
+
 
 	jmp exit
 
