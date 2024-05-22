8b25304c223792fafd2ef7bb6af31c6752635260
Rick-1242 <ricciueb@gmail.com>
2024-05-22 19:01:09 +0200

    boh

--- VR502617_VR501652/src/main.s
+++ VR502617_VR501652/src/main.s
@@ -2,9 +2,9 @@
 # .align 4	# do i need this?
 #---------Temp--------------
 fd: .int 0
-buffer: .string ""       # Spazio per il buffer di input
-newline: .byte 10        # Valore del simbolo di nuova linea
-lines: .int 0            # Numero di linee
+buffer: .ascii ""       # Spazio per il buffer di input
+newline: .byte 10       # Valore del simbolo di nuova linea
+sep: .byte 44			# Ovvero ","
 #---------Testo-------------
 menu: .ascii "Scelga l'algoritmo o exit:\n1. Earliest Deadline First (EDF)\n2. Highest Priority First (HPF)\n3. Exit\n"
 menu_len: .long . - menu
@@ -20,8 +20,8 @@ noArgsExitmsg_len: .long . - noArgsExitmsg
 
 #---------Offset------------
 TOTAL_OBJECTS = 10
-OBJECT_SIZE = 4		# Numero di interi(elemnti) per oggeto(ordine) 4 elementi x 1 byte = 4 byte a oggetto
-
+OBJECT_SIZE = 4		# Numero di interi(elemnti) per oggeto(ordine) 
+					# 4 elementi x 1 byte = 4 byte a oggetto
 IDENTIFICATIVO_OFFSET = 0
 DURATA_OFFSET = 1
 SCANDEZA_OFFSET = 2
@@ -38,6 +38,11 @@ ordiniArr: #.fill 40, 1, 0	# create 40 1 byte entries wiht 0 that will be modifi
 
 .section .bss
 	writeFile: .long 0
+	row: .int 0
+	col: .int 0
+	temp: .byte 0
+
+
 
 .section .text
 	.global _start
@@ -94,53 +99,78 @@ _openFile:
     mov $5, %eax        # syscall open
 						# Nome del file gia in ebx
     mov writeFile, %ecx	# Modalità di apertura (O_RDONLY)
-    int $0x80           # Interruzione del kernel
+    int $0x80
 
     # Se c'è un errore, esce
-    cmp $0, %eax
+    cmpl $0, %eax
     jl _noArgsExit
 
-    mov %eax, fd      # Salva il file descriptor in ebx
+    mov %eax, fd
+
+	# Riplusco le variabili per scrollare
+	movw $0, row
+	movw $0, col
 
  	# JMP to _readLoop or to _writeLoop based on writeFile
-	cmpl $1, writeFile			# Compare writeFile with 1
-    jl _readLoop				# Jump to _readLoop if writeFile < 1
+	cmpl $1, writeFile
+    jl _readLoop
     jmp _writeLoop   
 
 _closeFile: 
     mov $6, %eax        # syscall close
-    mov fd, %ecx      	# File descriptor
-    int $0x80           # Interruzione del kernel
+    mov fd, %ecx
+    int $0x80
 	jmp _exit   		# TODO: this is temporary. HAS TO BE REMOVED 
 
 _readLoop:		# Gets and converts the data from the file to our array.
     mov $3, %eax        # syscall read
     mov fd, %ebx        # File descriptor
-    mov $buffer, %ecx   # Buffer di input
-    mov $1, %edx        # Lunghezza massima
+    mov $buffer, %ecx
+    mov $1, %edx        # Lenght
     int $0x80
 
-    cmp $0, %eax        # ERROR or EOF check
+    cmpl $0, %eax        # ERROR or EOF check
     jle _closeFile
     
-    # Controllo se ho una nuova linea
-    movb buffer, %al    # copio il carattere dal buffer ad AL
-    cmpb newline, %al   # confronto AL con il carattere \n
-    jne _printChar     # se sono diversi stampo la linea
-    incw lines          # altrimenti, incremento il contatore
+    # New line check
+	xorl %ebx, %ebx
+    movb buffer, %bl
+    cmpb newline, %bl
+    jne _getLine
+    incw row
+	movw $0, col
 
 	# Put data into array
 	# jmp menu
 
-_printChar: # this will put data into array ig
+_getLine: # this will put data into array ig
 	pushl %edx
 	pushl $buffer
 	call _myPrint
 	addl $8, %esp
 
+	cmpb sep, %bl		# Check if the character is a separator
+    je _sepDetected		# If comma, move to the next string
+
+	# Not sure if it works we need eax to go into a temp variable
+
+	subb $48, %bl
+  	movl $10, %edx
+  	mulb %dl			# EBX = EBX * 10
+  	addl %ebx, temp		# Risultato é in temp
+
     jmp _readLoop      # Torna alla lettura del file
 
+_sepDetected:
+	xorl %eax, %eax
+	movl col, %eax
+	mov temp, ordiniArr + ??(,%eax, OBJECT_SIZE)		#NO GOOD
+	movb $0, temp
+	incw col
+	jmp _readLoop
+
 _writeLoop:	# Prints and converts the data form array to our file.
+	# ripulisci row e colonne(%ecx) first
 	# jmp closeFile
 	# jmp menu 
 
