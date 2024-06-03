92c050fd33a8e276390e08aca7149ed32ad96146
Rick-1242 <71984096+Rick-1242@users.noreply.github.com>
2024-06-03 22:46:32 +0200

    Addeed overflow check and changed count

--- VR502617_VR501652/src/general.s
+++ VR502617_VR501652/src/general.s
@@ -1,9 +1,13 @@
 .section .data # TODO: Rember to cean up all the push and pop before turnin in the project
 .section .text
     .globl _myPrint
-    # .globl _closeFile
+    .globl _mySTDERR
+    # .globl _openFile
     
-.type _myPrint, @function
+.type _myPrint, @function   # pushl msg_len     msg_len<int>
+	                        # pushl $msg        msg<char *>
+	                        # call _myPrint
+	                        # addl $8, %esp     reset esp
 _myPrint:
     push %ebp 
     movl %esp, %ebp 
@@ -27,20 +31,29 @@ _myPrint:
     pop %ebp 
     ret 
 
-# .type _closeFile, @function
-# _closeFile:
-#     push %ebp 
-#     movl %esp, %ebp 
-#     push %eax
-#     push %ecx
+.type _myPrint, @function   # pushl msg_len     msg_len<int>
+	                        # pushl $msg        msg<char *>
+	                        # call _myPrint
+	                        # addl $8, %esp     reset esp
+_mySTDERR:
+    push %ebp 
+    movl %esp, %ebp 
+	push %ebx
+	push %eax
+	push %ecx
+	push %edx
 
-#     movl $6, %eax        # syscall close
-#     movl 8(%ebp), %ecx
-#     int $0x80
+    # Write syscall
+    movl $4, %eax # syscall number for write()
+    movl $2, %ebx # file descriptor for stdout
+    movl 8(%ebp), %ecx # Address of string to write
+    movl 12(%ebp), %edx # number of bytes to write
+    int $0x80
 
-#     pop %ecx
-#     pop %eax
-#     movl %ebp, %esp 
-#     pop %ebp
-#     ret
-
+    pop %edx
+	pop %ecx
+	pop %eax
+	pop %ebx
+    movl %ebp, %esp 
+    pop %ebp 
+    ret
--- VR502617_VR501652/src/main.s
+++ VR502617_VR501652/src/main.s
@@ -17,6 +17,8 @@ penalty: .ascii "Penalty:"
 penalty_len: .long . - penalty
 noArgsExitmsg: .ascii "Si assicuri di specificare un filename, come argomento, e che questo esista\n"
 noArgsExitmsg_len: .long . - noArgsExitmsg
+overFlowDetectedmsg: .ascii "Overflow rilevato, si assicuri che i valori e la formattazione del file in input rispetti le specifiche del progetto\n"
+overFlowDetectedmsg_len: .long . - overFlowDetectedmsg
 
 #---------Offset------------
 TOTAL_OBJECTS = 10
@@ -27,20 +29,18 @@ DURATA_OFFSET = 1
 SCANDEZA_OFFSET = 2
 PRIORITA_OFFSET = 3
 
-ordiniArr: #.fill 40, 1, 0	# create 40 1 byte entries wiht 0 that will be modified by funcions
-							# So 1 object is 4 Bytes = .long = 32bits.
-	.rept TOTAL_OBJECTS
-		.byte 0  # field1 (1 byte)
-		.byte 1  # field2 (1 byte)
-		.byte 2  # field3 (1 byte)
-		.byte 3  # field4 (1 byte)
-	.endr
 
 .section .bss
-	tempRis: .byte 0
+	ordiniArr: #.fill 40, 1, 0	# create 40 1 byte entries wiht 0 that will be modified by funcions
+		# 1 object is 4 Bytes = .long = 32bits.
+		.rept TOTAL_OBJECTS
+			.byte 0  # IDENTIFICATIVO
+			.byte 0  # DURATA
+			.byte 0  # SCANDEZA
+			.byte 0  # PRIORITA
+		.endr
+	tempRis: .byte 0	# I valori in ordiniArr sono byte quindi anche questo.
 	writeFile: .long 0
-	row: .int 0			# legge solo 255 righe é una limitazione
-	col: .int 0			# TODO: maybe remove and use a reg
 
 
 
@@ -52,22 +52,24 @@ _start:
 	# movb ordiniArr + SCANDEZA_OFFSET(,%ebx, OBJECT_SIZE), %al
 	# call _itoa # for output
 	# call getArgs
-
 	## Get arguments
 	popl %ebx # Non ci serve
 	popl %ebx # argc[0]
 	popl %ebx # argc[1]
 	testl %ebx, %ebx
 	je _noArgsExit
+						# push %edx
+						# push (ordiniArr) / the address
+	jmp _openFile		# call _openFile would be cool and so openfile wopuld be in another file
 
-	jmp _openFile
-
-
-	# Open filename  DONE
-	# Read form file
-	# Put file into arry and convert shit with new fancy funcion
-	# Close file
 _menu:
+	leal ordiniArr(%ecx), %ebx
+	pushl %edx			# print(buffer)
+	pushl %ebx
+	call _myPrint
+	addl $8, %esp
+
+	jmp _exit
 
 	# menu [1,2,3]
 	# Run algo call  
@@ -75,7 +77,7 @@ _menu:
 	# popl %edx
 	# testl %edx, %edx
 	# je _noArgsExit
-	# incl writeFile
+	# inc writeFile
 	# jmp _open this time it opens as write
 	# The the printing to file ... and close
 	
@@ -85,31 +87,39 @@ _exit:
 	int $0x80
 
 _noArgsExit:		# Exit task for when Args is not provided or is wrong
-	movl $4, %eax
-	movl $2, %ebx
 	leal noArgsExitmsg, %ecx
-	movl noArgsExitmsg_len, %edx
-	int $0x80
+	pushl noArgsExitmsg_len
+	pushl %ecx
+	call _mySTDERR
+	addl $8, %esp 
 	jmp _exit
 
+_overFlowDetected:
+	leal overFlowDetectedmsg, %ecx
+	pushl overFlowDetectedmsg_len
+	pushl %ecx
+	call _mySTDERR
+	addl $8, %esp 
+	jmp _exit
 
 
 #------------File processing------------------- This might become all a big funcion that returns in _closeFile
 _openFile:
-	xorl %ecx, %ecx		# might not be nessecary TODO:
+	xorl %ecx, %ecx
     movl $5, %eax       # syscall open
 						# Nome del file gia in ebx
-    movb writeFile, %cl	# Modalità di apertura (O_RDONLY)
+    movl writeFile, %ecx	# Modalità di apertura (O_RDONLY) FIXME:
     int $0x80
 
     # Se c'è un errore, esce
-    cmpl $0, %eax
+    cmpl $0, %eax 
     jl _noArgsExit
 
     movl %eax, fd
 
- 	# JMP to _readLoop or to _writeLoop based on writeFile
-	cmpb $1, writeFile
+ 	xorl %ecx, %ecx 	# Clean ecx, used as counter in _readLoop TODO: this will be in the funcion
+	# JMP to _readLoop or to _writeLoop based on writeFile
+	cmpl $1, writeFile
     jl _readLoop
     jmp _writeLoop   
 
@@ -142,34 +152,31 @@ _readLoop:		# Gets and converts the data from the file to our array.
 	call _myPrint
 	addl $8, %esp
 
-	xorl %ebx, %ebx
-    movb buffer, %bl
+	# Check if char is (separator or \n)
+	xorl %ebx, %ebx		# FIXME: might remove, may not be necesary
+	movb buffer, %bl
     cmpb newline, %bl   # New line has to be treated like sep
-    je _sepDetected	 
-	cmpb sep, %bl		# Check if the character is a separator
-    je _sepDetected		# If sep, move to the next string
+    je _storeTemp	 
+	cmpb sep, %bl		
+    je _storeTemp		# If sep,storeTemp and skip char
 
 	# ascii -> int
 	subb $48, %bl
   	movl $10, %edx
-  	mulb %dl			# DL = DL * 10
-  	addb %bl, tempRis	# tempRis = tempRis + DL
+  	mulb %dl				# DL = DL * 10
+  	addb %bl, tempRis		# tempRis = tempRis + DL
+	jc _overFlowDetected	# Se il valore in tempRis supera 255 va in overflow
 
     jmp _readLoop
 
-_sepDetected:
+_storeTemp:
 	movb tempRis, %al
-	movb %al, ordiniArr(,%ecx, 1)	#NO GOOD It doesnt put it a the right place, we need to stop using tempRis and use %ecx cant move memory to memory
-	movb $0, tempRis				# Azzero tempRis per il prossimo valore e anche if ";;" allora assumo 0 DOCS
+	movb %al, ordiniArr(%ecx)		# Same as ordiniArr(,%ecx,1)
+	movb $0, tempRis				# Default value of tempRis is 0 so ";;" == ";0;" in the file
+	inc %ecx
 	jmp _readLoop
 
 _writeLoop:	# Prints and converts the data form array to our file.
-	# Riplusco le variabili per scrollare
-	movw $0, row
-	movw $0, col
-
-	# ripulisci row e colonne(%ecx) first
-	# jmp closeFile
-	# jmp menu 
+	jmp _closeFile
 
 # TASK 1 rewrite all funcions to return eax as by GCC calling conventions.
